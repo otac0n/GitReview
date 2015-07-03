1f70bdd32157917cbba84cf50624a8de031fd10d
John Gietzen <otac0n@gietzen.us>
2015-07-03 11:51:03 -0700

    Added an empty index page.

---
+++ GitReview/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HomeController.cs" company="(none)">
+//   Copyright © 2015 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GitReview.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The controller for the landing page for the application.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Gets the landing page of the application.
+        /// </summary>
+        /// <returns>A <see cref="ViewResult"/>.</returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
