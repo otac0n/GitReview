6b1d4194e3f6b5b58d25c7c86b0441a47bc9a066
John Gietzen <otac0n@gietzen.us>
2015-07-07 07:16:03 -0700

    Implemented a method to read revisions from the repository.

---
+++ GitReview/Models/Revision.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Revision.cs" company="(none)">
+//   Copyright © 2015 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GitReview.Models
+{
+    using LibGit2Sharp;
+
+    /// <summary>
+    /// Encapsulates the refs for a specific revision of a review.
+    /// </summary>
+    public class Revision
+    {
+        /// <summary>
+        /// Gets or sets the destination ref.
+        /// </summary>
+        public DirectReference Destination { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the revision.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source ref.
+        /// </summary>
+        public DirectReference Source { get; set; }
+    }
+}
--- GitReview/RepoFormat.cs
+++ GitReview/RepoFormat.cs
@@ -8,6 +8,10 @@
 
 namespace GitReview
 {
+    using System.Collections.Generic;
+    using GitReview.Models;
+    using LibGit2Sharp;
+
     /// <summary>
     /// Provides utility methods for working with the repository.
     /// </summary>
@@ -22,5 +26,38 @@ namespace GitReview
         /// Gets a string describing the format of source refs in the repository.
         /// </summary>
         public static readonly string SourceRef = "refs/heads/reviews/{id}/{version}/source";
+
+        /// <summary>
+        /// Gets all of the revisions in a review.
+        /// </summary>
+        /// <param name="repo">The repository.</param>
+        /// <param name="refPrefix">The review's ref prefix.</param>
+        /// <returns>An enumerable collection of revisions.</returns>
+        public static IEnumerable<Revision> GetRevisions(this Repository repo, string refPrefix)
+        {
+            var version = 1;
+            while (true)
+            {
+                var source = SourceRef.Replace("{id}", refPrefix).Replace("{version}", version.ToString());
+                var destination = DestinationRef.Replace("{id}", refPrefix).Replace("{version}", version.ToString());
+
+                var sourceRef = repo.Refs[source];
+                var destinationRef = repo.Refs[destination];
+
+                if (sourceRef == null || destinationRef == null)
+                {
+                    yield break;
+                }
+
+                yield return new Revision
+                {
+                    Id = version,
+                    Source = sourceRef.ResolveToDirectReference(),
+                    Destination = destinationRef.ResolveToDirectReference(),
+                };
+
+                version++;
+            }
+        }
     }
 }
