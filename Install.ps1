ad0471840252c492502267b935169e7d148eb2d2
John Gietzen <otac0n@gietzen.us>
2015-07-10 23:03:35 -0700

    Enhance the cr alias to allow specifying branches.

--- Install.ps1
+++ Install.ps1
@@ -2,4 +2,8 @@ $proj = (Get-Content .\GitReview\GitReview.csproj) -as [Xml]
 $url = $proj.Project.ProjectExtensions.VisualStudio.FlavorProperties.WebProjectProperties.IISUrl.TrimEnd('/') + '/new'
 "Url: $url"
 
-git config --global alias.cr "push $url @:refs/heads/source @{u}:refs/heads/destination"
+$command = "git push $url " + '${1:-HEAD}:refs/heads/source ${2:-$1@{u\}}:refs/heads/destination'
+$quoted = "'" + ($command -replace '\$', '\$') + "'"
+git config --global alias.cr "`"!sh -c '$command' --`""
+Write-Host -n "Git alias.cr: "
+git config --get alias.cr
